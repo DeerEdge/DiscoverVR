c7c3aabb0c6205733fddcdd0070f67400af7d804
Rushil Vislawath <rushil.vislawath@gmail.com>
2024-09-21 19:53:40 -0400

    version change

--- main.py
+++ main.py
@@ -23,7 +23,19 @@ def main():
 
     @router.add('/')
     def show_home():
-        ui.label('Featured Applications').classes('text-h5 q-px-md q-pt-md text-primary').style('font-weight: bold; color: #90caf9;')  # Subtle blue
+        with ui.carousel(animated=True, arrows=True, navigation=True).props('height=300px'):
+            for i in range(0, 60, 3):
+                with ui.carousel_slide():
+                    with ui.row(wrap=False):
+                            ui.html('''
+                                                <iframe src="https://deeredge.github.io/aframeSimulations/Fractals.html" width="1468px" height="200px"></iframe>
+                                            ''').style('padding: 8px;')
+
+                            ui.image('https://picsum.photos/id/32/270/180').classes('w-96')
+                            ui.image('https://picsum.photos/id/32/270/180').classes('w-96')
+                            ui.image('https://picsum.photos/id/32/270/180').classes('w-96')
+        ui.label('Featured Applications').classes('text-h5 q-px-md q-pt-md text-primary').style(
+            'font-weight: bold; color: #90caf9;')  # Subtle blue
         ui.label('Our pick of amazing WebAR experiences for you to explore — no app required!').classes(
             'text-body1 q-px-md').style('margin-bottom: 16px; color: #ffffff;')
         with ui.row().classes('q-gutter-sm q-px-md'):
@@ -32,11 +44,11 @@ def main():
                 ui.html('''
                     <iframe src="https://deeredge.github.io/aframeSimulations/FreeFallObjects.html" width="425px" height="200px"></iframe>
                 ''').style('padding: 8px;')
-                ui.label('Physics Free Fall Lab').classes('text-h6 q-pl-md').style('margin-top: -10px;')
+                ui.label('Biology Lab (Cell)').classes('text-h6 q-pl-md').style('margin-top: -10px;')
                 ui.label(
-                    'WebAR brings augmented reality directly to your browser, eliminating the need for apps. It allows '
-                    'users to interact with 3D objects and environments in real-time. With WebAR, businesses can create '
-                    'immersive experiences accessible to anyone with a smartphone.'
+                    'Explore the intricate world of biology in our VR Lab, where you can dive into a fully immersive 3D'
+                    'representation of a cell. Zoom in to see detailed structures like the nucleus, mitochondria, and'
+                    'more, as if you are inside the microscopic world of life itself!'
                 ).classes('q-pl-md').style('margin-top: -10px;')
                 ui.button('Enter Experience', on_click=lambda: router.open('/experience1')).classes('q-pl-lg').style('width: calc(100% - 10px); display: block; margin-top: -5px; margin-left: 5px; margin-bottom: 5px')
 
@@ -56,13 +68,13 @@ def main():
             # Card 3
             with ui.card().classes('q-pa-sm').style('width: 31.9%'):
                 ui.html('''
-                    <iframe src="https://deeredge.github.io/aframeSimulations/Fractals.html" width="425px" height="200px"></iframe>
+                    <iframe src="https://deeredge.github.io/aframeSimulations/3DShapes.html" width="425px" height="200px"></iframe>
                 ''').style('padding: 8px;')
-                ui.label('Fractal Lab').classes('text-h6 q-pl-md').style('margin-top: -10px;')
+                ui.label('3D Shapes Lab').classes('text-h6 q-pl-md').style('margin-top: -10px;')
                 ui.label(
-                    'WebAR brings augmented reality directly to your browser, eliminating the need for apps. It allows '
-                    'users to interact with 3D objects and environments in real-time. With WebAR, businesses can create '
-                    'immersive experiences accessible to anyone with a smartphone.'
+                    'Explore the world of geometry in our VR Lab, where you can interact with various 3D shapes and '
+                    'discover their volume and surface area formulas. Watch as the shapes come to life, and gain a '
+                    'deeper understanding of how these mathematical concepts apply in three dimensions!'
                 ).classes('q-pl-md').style('margin-top: -10px;')
                 ui.button('Enter Experience', on_click=lambda: router.open('/experience3')).classes('q-pl-lg').style('width: calc(100% - 10px); display: block; margin-top: -5px; margin-left: 5px; margin-bottom: 5px')
 
@@ -88,14 +100,27 @@ def main():
         with ui.column().classes('w-full q-pa-md'):
             ui.button('Back', on_click=lambda: router.open('/')).classes('q-mb-md')
             ui.html('''
-                <iframe src="https://deeredge.github.io/aframeSimulations/Fractals.html" width= "1420px" height="700px"></iframe>
+                <iframe src="https://deeredge.github.io/aframeSimulations/3DShapes.html" width= "1420px" height="700px"></iframe>
             ''')
 
     @router.add('/resources')
     def show_resources():
         with ui.column().classes('w-full q-pa-md'):
-            ui.label('Resources').classes('text-h4 q-mb-md text-primary')
-            ui.label('Here you can find various resources and materials.').classes('text-body1')
+            ui.label('Explore/Learn').classes('text-h4 q-mb-md text-primary')
+            ui.separator().style(f"background-color: #90D5FF")
+            with ui.row():
+                ui.button('Art', on_click=lambda: router.open('/art'))
+                ui.button('Math', on_click=lambda: router.open('/math'))
+                ui.button('Biology', on_click=lambda: router.open('/biology'))
+                ui.button('Astronomy', on_click=lambda: router.open('/astronomy'))
+            ui.separator().style(f"background-color: #90D5FF")
+
+    @router.add('/art')
+    def show_art():
+        with ui.column().classes('w-full q-pa-md'):
+            ui.label('Explore Art Topics').classes('text-h4 q-mb-md text-primary')
+
+
 
     @router.add('/about')
     def show_about():
@@ -103,11 +128,18 @@ def main():
             ui.label('About Us').classes('text-h4 q-mb-md text-primary')
             ui.label('Learn more about our company and mission.').classes('text-body1')
 
-    @router.add('/demo')
-    def show_demo():
+    @router.add('/sandbox')
+    def show_sandbox():
         with ui.column().classes('w-full q-pa-md'):
-            ui.label('Demo').classes('text-h4 q-mb-md text-primary')
-            ui.label('Explore our product demos and features.').classes('text-body1')
+            ui.label('SandBox').classes('text-h4 q-mb-md text-primary')
+            ui.label('Play around with prompts!').classes('text-body1')
+        with ui.row().classes('q-gutter-sm q-px-md'):
+            # Card 1
+            with ui.card().classes('q-pa-sm').style('width: 31.9%'):
+                ui.html('''
+                    <iframe src="https://www.perplexity.ai/" width="425px" height="200px"></iframe>
+                ''').style('padding: 8px;')
+
 
     with ui.header().classes(replace='row items-center justify-between q-py-md bg-dark').style(
             'border-bottom: 1px solid #333;'):
@@ -118,13 +150,13 @@ def main():
             tab_home = ui.tab('Home').classes('text-h6')
             tab_resources = ui.tab('Resources').classes('text-h6')
             tab_about = ui.tab('About').classes('text-h6')
-            tab_demo = ui.tab('Demo').classes('text-h6')
+            tab_sandbox = ui.tab('sandbox').classes('text-h6')
 
     # Define click handlers for tabs
     tab_home.on('click', lambda: router.open('/'))
     tab_resources.on('click', lambda: router.open('/resources'))
     tab_about.on('click', lambda: router.open('/about'))
-    tab_demo.on('click', lambda: router.open('/demo'))
+    tab_sandbox.on('click', lambda: router.open('/sandbox'))
 
     # this places the content which should be displayed
     router.frame().classes('w-full p-4')
